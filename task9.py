7836bf01fc12db25556a1799a3c5b036674fac50
Gontse Mfete <gontsemfete@gmail.com>
2021-10-25 07:27:07 +0200

    lower case commit

--- task9.py
+++ task9.py
@@ -1,8 +1,7 @@
 def vowel(x): 
-    x="heluulooo"
-    y= {i for i in x if i in 'a,e,i,o,u'}
+    y= {i for i in x.lower() if i in 'a,e,i,o,u'}
     if y:
         print("Vowels:",*y)
     else:
         print("not present")
-vowel(x)
+vowel("HelloUU")
